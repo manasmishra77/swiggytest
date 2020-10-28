da1a678144258d83cecb091c4166998361e23b25
Manas Mishra <manas1.mishra@ril.com>
2020-10-28 11:43:41 +0530

    Initial Commit

---
+++ SwiggyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiggyTest
+//
+//  Created by Manas1 Mishra on 28/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
